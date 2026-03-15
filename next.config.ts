cf30784253ce7baf0d5e657d5eb3c4481d456aa9
s1nful2k25 <pokerknabe@gmail.com>
2026-03-15 01:01:29 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-
+  // Spart RAM: Ignoriert ESLint-Fehler beim Build im Docker
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // Spart massiv RAM: Ignoriert TypeScript-Fehler beim Build im Docker
   typescript: {
     ignoreBuildErrors: true,
   },
-
 };
 
 export default nextConfig;
